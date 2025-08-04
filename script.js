c9a195b41d5fb7a5e8f35adfd03f9ab64eece42a
Vutlhari-Mabasa <162843020+Vutlhari-Mabasa@users.noreply.github.com>
2025-08-04 21:26:11 +0200

    Initial setup for Unsplash image search: added fetch logic and form handler

---
+++ script.js
@@ -0,0 +1,31 @@
+const accessKey = "-byo0uV3a5EGe8ZsU3HgdNgDnsfiZD8JtqxgC5-vRuo"
+
+
+
+
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
+
+async function searchImages() {
+    keyword = searchBox.ariaValueMax;
+    const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}';
+
+const response = await fetch(url);
+const data = await response.json();
+
+console.log(data);
+
+}
+
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    page = 1;
+    searchImages();
+})
+
