d85154d89242129c51a831aea2ccf9a5bd51f74e
Vutlhari-Mabasa <162843020+Vutlhari-Mabasa@users.noreply.github.com>
2025-08-05 01:06:28 +0200

    added funnctionality for the showmorebtn to make sure it can be displayed and used when needed

--- script.js
+++ script.js
@@ -14,10 +14,16 @@ let page = 1;
 
 async function searchImages() {
     keyword = searchBox.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}`;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
 
 const response = await fetch(url);
 const data = await response.json();
+
+
+if(page === 1){
+    searchResult.innerHTML = "";
+}
+
 
 const results = data.results;
 
@@ -32,11 +38,19 @@ results.map((result) =>{
     searchResult.appendChild(imageLink);
     })
 
+    showMoreBtn.style.display = "block";
+
 }
 
 searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
     page = 1;
+    searchImages();
+})
+
+
+showMoreBtn.addEventListener("click", () =>{
+    page++;
     searchImages();
 })
 
